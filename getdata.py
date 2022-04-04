751fa1bca48bb0c28a1b0e16685b30f9e34462d6
OI-Master <oimasterfake@gmail.com>
2022-04-04 18:41:37 +0900

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -8,7 +8,7 @@ try:
     res = "{}，全国累计确诊病例 {} 例，死亡病例 {} 例，治愈 {} 例。现有确诊病例 {} 例，疑似病例 {} 例。".format(r.get('times'), r.get('gntotal'), r.get('deathtotal'), r.get('curetotal'), r.get('econNum'), r.get('sustotal'))
     print(res)
     with open('README.md', 'w') as f:
-        f.write('# 中国新型冠状病毒确诊情况')
+        f.write('# 中国新型冠状病毒确诊情况\n')
         f.write(res)
 
 except:
